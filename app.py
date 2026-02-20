7c12858e4a2f4e97136342efa4e5760952d2b168
tlincheck <tlincheck@gmail.com>
2026-02-20 12:15:27 -0600

    Modified files

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from dash import Dash, html, dcc, callback, Output, Input
 import plotly.express as px
 import pandas as pd
 
+
+from dash import Dash, html, dcc, callback, Output, Input
+# import dash_core_components as dcc
+# import dash_html_components as html
+# from dash.dependencies import Input, Output
 # df = pd.read_csv()
 
 
